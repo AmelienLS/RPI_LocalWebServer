1d31413ab5acd0b46ac5ebdb189ac04e947e1dc2
Amelienls <laradesimiamelien@gmail.com>
2026-02-27 08:24:17 +0100

    fix(launcher)

--- Setups Windows/Lancer.bat
+++ Setups Windows/Lancer.bat
@@ -2,33 +2,25 @@
 chcp 65001 >nul
 setlocal
 
-:: Résolution du chemin absolu du projet (dossier parent de ce script)
-for %%I in ("%~dp0..") do set "PROJECT_DIR=%%~fI"
+:: Variables
+set "PROJECT_DIR=%USERPROFILE%\RPI_LocalWebServer-Release"
 set "VENV_DIR=%PROJECT_DIR%\venv"
 
 cls
-echo [>>>] Démarrage de l'application...
-echo     Projet : %PROJECT_DIR%
-echo     URL    : http://127.0.0.1:5000
+echo [>>>] Démarrage du serveur WSGI local...
+echo     URL : http://127.0.0.1:5000
 echo [!] CTRL+C pour arrêter.
 echo.
-
-if not exist "%VENV_DIR%\Scripts\activate.bat" (
-  echo [!] Environnement virtuel introuvable : %VENV_DIR%
-  echo     Lancez d'abord Démarrage.bat pour installer l'application.
-  pause & exit /b 1
-)
 
 call "%VENV_DIR%\Scripts\activate.bat"
 pushd "%PROJECT_DIR%"
     start "" powershell -Command "Start-Sleep -Seconds 3; Start-Process 'http://127.0.0.1:5000'"
     python -m waitress --host=127.0.0.1 --port=5000 APP:app
     if errorlevel 1 (
-        echo.
-        echo [!] Le serveur s'est arrêté avec une erreur.
+        echo [!] Le serveur s'est arrêté avec une erreur !
+        pause
     )
 popd
 call "%VENV_DIR%\Scripts\deactivate.bat"
 
-pause
 endlocal
