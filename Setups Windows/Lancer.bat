5e8ff08fa3c8ae74c8fc13a03d724d606ee48381
AmelienLS <laradesimiamelien@gmail.com>
2026-02-27 10:33:38 +0100

    fix(lancer)

--- Setups Windows/Lancer.bat
+++ Setups Windows/Lancer.bat
@@ -5,29 +5,31 @@ setlocal
 set "PROJECT_DIR=%USERPROFILE%\RPI_LocalWebServer-Release"
 set "VENV_DIR=%PROJECT_DIR%\venv"
 
+cls
+echo [i] Lancement de l'application...
+echo.
+
 if not exist "%VENV_DIR%\Scripts\activate.bat" (
-  echo [!] Environnement virtuel introuvable : %VENV_DIR%
-  echo     Lancez d'abord Démarrage.bat pour installer l'application.
-  pause & exit /b 1
+    echo [!] Environnement virtuel introuvable : %VENV_DIR%
+    echo     Lancez d'abord "Setups Windows\Démarrage.bat" pour installer l'application.
+    pause & exit /b 1
 )
 
-cls
-echo [>>>] Démarrage du serveur WSGI local...
-echo     Projet : %PROJECT_DIR%
-echo     URL    : http://127.0.0.1:5000
+call "%VENV_DIR%\Scripts\activate.bat"
+
+echo [>>>] Démarrage du serveur...
+echo     URL : http://127.0.0.1:5000
 echo [!] CTRL+C pour arrêter.
 echo.
 
-call "%VENV_DIR%\Scripts\activate.bat"
+start "" powershell -Command "Start-Sleep -Seconds 3; Start-Process 'http://127.0.0.1:5000'"
 pushd "%PROJECT_DIR%"
-    start "" powershell -Command "Start-Sleep -Seconds 3; Start-Process 'http://127.0.0.1:5000'"
     python -m waitress --host=127.0.0.1 --port=5000 APP:app
     if errorlevel 1 (
-        echo.
-        echo [!] Le serveur s'est arrete avec une erreur.
+        echo [!] Le serveur s'est arrête avec une erreur !
+        pause
     )
 popd
-call "%VENV_DIR%\Scripts\deactivate.bat"
 
-pause
+call "%VENV_DIR%\Scripts\deactivate.bat"
 endlocal
